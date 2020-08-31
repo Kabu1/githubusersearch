7015f82088ae3ecaa82e239eecb44482a0893d8a
kabu1 <kkabubenson@gmail.com>
2020-08-31 04:44:44 -0400

    initial commit

---
+++ Github/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+    NavbarComponent,
+    UserInfoComponent,
+    RepositoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
